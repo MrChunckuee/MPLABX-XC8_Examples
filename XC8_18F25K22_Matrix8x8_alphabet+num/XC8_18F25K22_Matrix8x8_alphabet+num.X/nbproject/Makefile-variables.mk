2c8b0ee958d84783215d63d6ac50378c503687dc
MrChunckuee <mrchunckuee.psr@gmail.com>
2020-06-24 01:07:17 -0500

    Matriz de LEDs 8x8: Mostrar abecedario y numeros

---
+++ XC8_18F25K22_Matrix8x8_alphabet+num/XC8_18F25K22_Matrix8x8_alphabet+num.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XC8_18F25K22_Matrix8x8_alphabet_num.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XC8_18F25K22_Matrix8x8_alphabet_num.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xc818f25k22matrix8x8alphabet+num.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xc818f25k22matrix8x8alphabet+num.x.tar
