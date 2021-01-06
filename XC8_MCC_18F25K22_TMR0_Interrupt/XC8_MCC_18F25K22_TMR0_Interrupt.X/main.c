9bc95505838be10aa36d5ad443f9083fe0c8d641
MrChunckuee <mrchunckuee.psr@gmail.com>
2021-01-06 01:00:20 -0600

    Interrupcion por desbordamiento del TIMER 0

---
+++ XC8_MCC_18F25K22_TMR0_Interrupt/XC8_MCC_18F25K22_TMR0_Interrupt.X/main.c
@@ -0,0 +1,97 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.6
+        Device            :  PIC18F25K22
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+
+/*
+                         Main application
+ */
+
+/*******************************************************************************
+ *
+ *                  Interrupción por desbordamiento del TMR0
+ *
+ *******************************************************************************
+ * Author:          Pedro Sánchez (MrChunckuee)
+ * Blog:            http://mrchunckuee.blogspot.com/
+ * Email:           mrchunckuee.psr@gmail.com
+ * Description:     Uso de la herramienta MCC de microchip para generar un desbordamiento 
+ *                  en el timer 0 y atenderlo por medio de una interrupcion, en ella se 
+ *                  cambia el estado de un LED conectado al pin RC7 cada 500ms, en el main 
+ *                  pincipal se deja cambiando el estado del pin RC7 cada 250ms.
+ *******************************************************************************
+ * Rev.         Date            Comment
+ *   v0.0.1     06/01/2021      Creación del ejemplo.
+ ******************************************************************************/
+
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    while (1)
+    {
+        // Add your application code
+        LED_Status_RC7_Toggle();
+        __delay_ms(250);
+    }
+}
+/**
+ End of File
+*/
---
+++ XC8_MCC_18F25K22_TMR0_Interrupt/XC8_MCC_18F25K22_TMR0_Interrupt.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XC8_MCC_18F25K22_TMR0_Interrupt.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XC8_MCC_18F25K22_TMR0_Interrupt.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xc8mcc18f25k22tmr0interrupt.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xc8mcc18f25k22tmr0interrupt.x.tar
